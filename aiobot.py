671eb65ced6242a76bf599a1fe93653a1441b0cc
Airakuu <tisinn5@gmail.com>
2026-03-08 23:48:09 +0300

    Добавил комментарии, чтобы не забыть как все работает

--- aiobot.py
+++ aiobot.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher # импорт для создания бота и диспетчера, Bot для создания экземпляра бота, Dispatcher для обработки сообщений
 from aiogram.types import Message
 from handlers import user
 from config import TOKEN
@@ -7,10 +7,10 @@ async def main():
     bot = Bot(token=TOKEN) #импорт токена из config.py, чтобы не светить его в открытом коде(config.py добавлен в .gitignore)
     dp = Dispatcher()
     dp.include_router(user)
-    await dp.start_polling(bot)
+    await dp.start_polling(bot) # запуск бота, который будет обрабатывать сообщения и коллбеки, используя диспетчер
     
     
-if __name__ == "__main__":
+if __name__ == "__main__": # чтобы бот работал
     try:
         import asyncio
         asyncio.run(main())
